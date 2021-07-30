5d0d43066f8932dc0a95e73b71a02563973a91ee
Mao <maokitty23k@gmail.com>
2021-07-30 16:22:32 -0400

    edited

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import "./App.css";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
-  return <div></div>;
+
+  useEffect(() => {
+    axios({
+      url: ' https://api.tvmaze.com/shows/139/images',
+      method: 'GET',
+      dataResponse: 'json',
+      params: {
+        api_key: 'Y6sBDEBiE9eU7nhyUf5NOycV9IzoiwYQ',
+        q: 'girls'
+      }
+    }).then((res) => {
+      console.log(res);
+    })
+  })
+
+  return  (
+  <div>
+    <h1>HELLO</h1>
+  </div>
+
+  )
 }
 
 export default App;
